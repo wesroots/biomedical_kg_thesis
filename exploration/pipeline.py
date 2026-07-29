bd92ead0c3b293129807328ebad4a996130ef1f5
wesroots <wesleyroots954@outlook.com>
2026-07-29 20:01:32 +0100

    Updates and backup before modularisation of main notebook

---
+++ exploration/pipeline.py
@@ -0,0 +1,10 @@
+from parsing import parse_output
+from evaluation import evaluation_v1, evaluation_v2, evaluation_v3, evaluation_v4
+from exporting import export_run
+
+def process_run(
+        output_dict: dict,
+
+):
+    evaluator = 
+    pass
